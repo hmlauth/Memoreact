b49c6fd1b6f0dbe5aaaeab20bf549bda96e78543
Andromeda <hannalauth@gmail.com>
2019-04-27 13:23:23 -0700

    Updates

---
+++ precache-manifest.4b493714cefa8b27093b04faf378036f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "511a3d73e3016561b09272bed126cbdc",
+    "url": "/Memoreact/index.html"
+  },
+  {
+    "revision": "45c9a86f39dc06db1093",
+    "url": "/Memoreact/static/css/main.8222a209.chunk.css"
+  },
+  {
+    "revision": "a686e41154fd92225c54",
+    "url": "/Memoreact/static/js/2.5bfccd3c.chunk.js"
+  },
+  {
+    "revision": "45c9a86f39dc06db1093",
+    "url": "/Memoreact/static/js/main.64f4a1e5.chunk.js"
+  },
+  {
+    "revision": "a93d49e05e9b84f89b74",
+    "url": "/Memoreact/static/js/runtime~main.01f4c209.js"
+  }
+]);
